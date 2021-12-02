5f7ea692bd22ac66deb3bae97a6f5f67e0012e5a
meer <71112748+mirnumaan@users.noreply.github.com>
2021-12-02 09:44:21 +0530

    Add files via upload

---
+++ amazon_clone_two/firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA1GhRkGd5K9g3jRrw8C72A_Z-kdh-xCgQ",
+    authDomain: "clone-two-8b27f.firebaseapp.com",
+    projectId: "clone-two-8b27f",
+    storageBucket: "clone-two-8b27f.appspot.com",
+    messagingSenderId: "768269085051",
+    appId: "1:768269085051:web:2d73be932d75c8591c35de",
+    measurementId: "G-41KKP0VB3K"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
