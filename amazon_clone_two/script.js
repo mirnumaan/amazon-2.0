ae1f492e6a34d3c619ae05ca1034d6057f6ff7c2
numaan bashir mir <mirnumaan06@gmail.com>
2021-12-10 19:17:11 +0530

    ok

--- amazon_clone_two/script.js
+++ amazon_clone_two/script.js
@@ -7,7 +7,7 @@ function getItems(){
             items.push({
                 id: doc.id,
                 image:doc.data().image,
-                // name:doc.data().name,
+                name:doc.data().name,
                 make:doc.data().make,
                 rating:doc.data().rating,
                 price:doc.data().price
@@ -19,35 +19,56 @@ function getItems(){
        
     });
 }
+function addToCart(item){
+    console.log(item);
+   db.collection("cart-items").doc(item.id)
+   .set({
+       image:item.image,
+        make:item.make,
+       rating:item.rating,
+       price:item.price,
+       name:item.name
+   })
+//    db.collection("cart-items").add({
+// //         image:item.image,
+// //        make:item.make,
+// //        rating:item.rating,
+// //        price:item.price
+// // })
+}
     function generateItems(items){
         let itemsHTML="";
         items.forEach((item) =>{
             let doc = document.createElement("div");
             doc.classList.add("main-product", "mr-5");
             doc.innerHTML=`
+            
             <div class="product-image rounded-lg w-48 h-52  p-4 ">
-            <img class= "w-full h-full object-contain"src="${item.image}">
-        </div>
-        <div class="product-name text-gray-600 font-bold mt-2 test-sm cursor-pointer">
-            ${item.name}
+                <img class= "w-full h-full object-contain" src="${item.image}">
+            </div>
+            <div class="product-name text-gray-600 font-bold mt-2 text-sm cursor-pointer">
+                ${item.name}
 
-        </div>
-        <div class="product built text-bold text-green-500 my-1">
-            ${item.make}
+            </div>
+            <div class="product-built text-bold text-green-500 my-1">
+                ${item.make}
 
-        </div>
-        <div class="product-rating text-black my-1">
-            ⭐️⭐️⭐️⭐️⭐️ ${item.rating}
+            </div>
+            <div class="product-rating text-black my-1">
+                ⭐️⭐️⭐️⭐️⭐️ ${item.rating}
 
-        </div>
+            </div>
             <div class="product-price text-black  my-1">
-            $ ${item.price}
+                $ ${item.price}
             </div>
            
         `
         let addToCartEl = document.createElement("div")
         addToCartEl.classList.add("hover:bg-yellow-600", "cursor-pointer", "product-add", "h-8", "w-28", "rounded", "bg-yellow-500", "text-white", "text-md", "flex", "justify-center", "items-center");
         addToCartEl.innerText="Add To Cart";
+        addToCartEl.addEventListener("click", function(){
+            addToCart(item)
+        })
         doc.appendChild(addToCartEl);
         document.querySelector(".main-section-products").appendChild(doc);
         })
